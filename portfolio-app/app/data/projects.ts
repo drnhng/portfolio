696395e1eee63e42fbd70c1d83dc39f25333024e
drnhng <huangdarren1106@gmail.com>
2025-03-11 13:42:58 -0700

    koodos project added

--- portfolio-app/app/data/projects.ts
+++ portfolio-app/app/data/projects.ts
@@ -1,25 +1,35 @@
 export const projects = [
     {
       title: "Silknode: Social Curation Site MVP",
-      description: "Social curation site MVP",
-      longDescription: "A comprehensive social platform that allows users to curate and share content. I led a team of three engineers to build Silknode, a social curation app, from the ground up. I designed the architecture and database, translated Figma designs into interactive React components, and optimized performance with caching, SSR, and lazy loading.",
+      description: "",
+      longDescription: "A comprehensive social platform that allows users to curate and share content. I led a team of three engineers to build the MVP of Silknode, a social curation app, from the ground up. I designed the architecture and database, translated Figma designs into interactive React components, and optimized performance with caching, SSR, and lazy loading.",
       images: ["/images/projects/silknodeimages/silknode.png", "/images/projects/silknodeimages/sn0.png", "/images/projects/silknodeimages/sn1.png", "/images/projects/silknodeimages/sn2.png", "/images/projects/silknodeimages/sn3.png"],
       link: "https://bones.silk.cx",
-      technologies: ["React", "Next.js", "PostgreSQL", "AWS"],
+      technologies: ["React", "Next.js", "JavaScript/HTML", "TailwindCSS", "PostgreSQL", "AWS"],
       slug: "silknode"
     },
     {
       title: "ArchivePDF: Progressive Web App",
-      description: "Progressive web app for PDF management",
+      description: "",
       longDescription: "A collaborative venture dedicated to sharing archive fashion scans, articles, and content with the world. I helped build the backend and frontend of the site, and am currently working on our next project, a SaaS to help creators network. I'm also a writer for the team!",
       images: ["/images/projects/archivepdfimages/archivepdf.png", "/images/projects/archivepdfimages/a0.png", "/images/projects/archivepdfimages/a1.png", "/images/projects/archivepdfimages/a2.png", "/images/projects/archivepdfimages/a3.png"], 
       link: "https://beta.archivepdf.net/",
-      technologies: ["Node.js", "TypeScript", "Firebase", "Stripe"],
+      technologies: ["React", "JavaScript", "HTML/TailwindCSS", "Node.js", "TypeScript", "Firebase"],
       slug: "archivepdf"  
     },
+    {
+        title: "Koodos Labs Internship: Various Sites",
+        description: "",
+        longDescription: "Various sites I built during my internship at Koodos Labs. I translating Figma designs from the Product Designer into React websites. These sites were then launched on the Koodos tiktok page to gain user signups for the product. By the end of my internship, theses sites brought on a total of 77K+ user signups for the main app.",
+        images: ["/images/projects/koodoslabsimages/k1.png", "/images/projects/koodoslabsimages/k0.png", "/images/projects/koodoslabsimages/k4.png","/images/projects/koodoslabsimages/k2.png" ,"/images/projects/koodoslabsimages/k3.jpg"],
+        link: "",
+        technologies: ["React", "HTML/CSS", "Redux", "Figma"],
+        slug: "koodoslabs"
+
+    },
     {  
-      title: "Spotify Pie: Viral App",  
-      description: "Viral music visualization app",
+      title: "Spotify Pie: Viral Website",  
+      description: "",
       longDescription: "A music visualization site that generates genre-based pie charts from Spotify listening data. I built the app from the ground up, integrating users' audio data to create dynamic, shareable charts. It quickly went viral, gaining over 12 million views in its first week and being featured in Yahoo Finance, USA Today, MetroUK, and IMDb. After a successful 2 years run, it was acquired by SENTRY Co in 2024.",
       images: ["/images/projects/spotifypieimages/spotifypie.png", "/images/projects/spotifypieimages/spotifypie.png", "/images/projects/spotifypieimages/spotifypie.png"],
       link: "https://project3.com",
